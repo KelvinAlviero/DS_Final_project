b64ce4ce9b065c45c7395d294fb08b8b3b4686b2
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2024-06-11 14:52:47 +0700

    Yippee

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,85 +1,33 @@
-import javax.swing.JFrame;
-import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.image.BufferStrategy;
-
-import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
 
 //Main game loop with extra funnies
-public class Main extends Canvas implements Runnable {
+public class Main extends JFrame {
 
     private static final int WIDTH = 1200;//Widht
     private static final int HEIGHT = 600;//Height
-    private Thread app_thread; //Used for starting the whole ordeal
-    private boolean app_running = false; //checks if the window is running
+
+
 
     public Main() {
-        Dimension size = new Dimension(WIDTH, HEIGHT);
-        setPreferredSize(size);
-
-        //load background and spritesheet stuff
-        setFocusable(true);
-
+        setTitle("Mark-2C 'Calender', Mod. 2024");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(new Color(227,174,87));
+        setLocationRelativeTo(null);
+        JPanel panel = new JPanel();
+        add(panel, BorderLayout.SOUTH);
     }
 
-    public synchronized void start() {
-        if (app_running) return;
-        app_running = true;
-        app_thread = new Thread(this);
-        app_thread.start();
-    }
 
-    public synchronized void stop() {
-        if (!app_running) return;
-        app_running = false;
-        try {
-            app_thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void run() {
-        long lastTime = System.nanoTime();
-        final double nsPerUpdate = 1000000000.0 / 60.0; // 60 FPS
-        long lastTimer = System.currentTimeMillis();
-        double delta = 0;
-
-        while (app_running) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / nsPerUpdate;
-            lastTime = now;
-
-            while (delta >= 1) {
-                delta--;
-            }
-
-            render();
-
-            if (System.currentTimeMillis() - lastTimer >= 1000) {
-                lastTimer += 1000;
-            }
-        }
-    }
-
-    private void render() { // You render and put stuff here
-        BufferStrategy bufferStrategy = getBufferStrategy();
-        if (bufferStrategy == null) {
-            createBufferStrategy(3);
-            return;
-        }
-    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Mark-2C 'Calender', Mod. 2024");
-        Main Calender_App = new Main();
-        frame.add(Calender_App);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main().setVisible(true);
 
-        Calender_App.start();
+            }
+        });
     }
 }
